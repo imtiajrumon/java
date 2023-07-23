8daef487903a8bf674be32306c6dadbb52c7cae2
imtiajrumon <128974739+imtiajrumon@users.noreply.github.com>
2023-07-23 03:38:41 -0700

    Add files via upload

---
+++ js/saipt.js
@@ -0,0 +1,32 @@
+function textChange(){
+
+	document.getElementById('h2').innerHTML = "Im Imtiaj Rumon";
+}
+
+// show date
+function showDate(){
+	document.getElementById('ddh2').innerHTML = Date();
+}
+// variable
+function lightOn(){
+	document.getElementById('myImage').src='images/on.gif'
+}
+function lightOff(){
+	document.getElementById('myImage').src='images/light_off.gif'
+}
+// font size
+function fontSize(){
+	document.getElementById('demo').style.fontSize='35px'
+}
+// none
+function none(){
+	document.getElementById('demo1').style.display='none'
+}
+// plus2
+function plus(){
+	document.write(5 + 6)
+}
+// plus intu
+function demo5(){
+	document.getElementById("demo5").innerHTML ="The value of z is " + z + ".";
+}
