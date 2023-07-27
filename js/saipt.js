1babe6ba14d72d51035a75e6d83e471c1cb5c55a
imtiajrumon <128974739+imtiajrumon@users.noreply.github.com>
2023-07-27 06:33:55 -0700

    Add files via upload

--- js/saipt.js
+++ js/saipt.js
@@ -28,5 +28,200 @@ function plus(){
 }
 // plus intu
 function demo5(){
-	document.getElementById("demo5").innerHTML ="The value of z is " + z + ".";
+	let number1, number2, result;
+	number1 = 55;
+	number2 = 12;
+	result = number1 - number2
+	document.getElementById("result1").innerHTML = result;
+}
+// no
+function outPut(){
+	var number1 = 10;
+	var number2 = 20;
+
+	var result = number1 + number2;
+
+	document.getElementById('op').innerHTML =result;
+}
+// demo
+function divid(){
+	document.getElementById('demo6').innerHTML = (21 / 4);
+}// demo
+function divi(){
+	let x,y;
+	x = 71;
+	y = 12;
+	document.getElementById('demo7').innerHTML = (x / y);
+}
+// demo
+function div(){
+	let x,y;
+	x = 71;
+	y = 12;
+	document.getElementById('demo8').innerHTML = (21 * 4);
+}
+// demo
+function di(){
+	let x,y;
+	x = 71;
+	y = 12;
+	document.getElementById('demo9').innerHTML = (21 - 4);
+}
+// demo
+function d(){
+	let a,x,y,z;
+	a = 100;
+	x = 71;
+	y = 12;
+	z = a + y;
+	document.getElementById('demo10').innerHTML = (z - x);
+}
+// demo
+function single(){
+	let x,y,z;
+	x = 71;
+	y = 12;
+	z = x + y;
+	document.getElementById('demo11').innerHTML = z;
+}
+// demo
+function singl(){
+	document.getElementById('demo12').innerHTML = 10.50;
+}
+// demo
+function sing(){
+	let x;
+	x = 555; 
+	document.getElementById('demo13').innerHTML = x;
+}
+// demo
+function sin(){
+	let x,y,z,a;
+	x = 555;
+	y = 444;
+	z = 44;
+	a = 4;
+	document.getElementById('demo14').innerHTML = ((x-y)+z)*a;
+}
+// demo
+function si(){
+	let x;
+	x = 555;
+	document.getElementById('demo15').innerHTML = x * 10;
+}
+// demo
+function freeFire(){
+	let x,y;
+	x = 555 - 55;
+	y = x * 10
+	document.getElementById('demo16').innerHTML = x * 10;
+}
+// demo
+function freeFir(){
+	let x,y;
+	x = 105 - 55;
+	y = x * 10
+	document.getElementById('demo17').innerHTML = x * 10;
+}
+// demo
+function freeFi(){
+	const price1 = 5;
+	const price2 = 55;
+	let total = price1 + price2;
+	document.getElementById('demo18').innerHTML = "The total is: " + total;
+}
+// demo
+function freeF(){
+	const price1 = 3.15;
+	let person = "John Doe";
+	let answer = 'Yes I am Rumon!';
+	document.getElementById('demo19').innerHTML = price1 + "<br>" + person + "<br>" + answer;
+}
+// demo
+function free(){
+	let p = 5 + 5 + 5;
+	document.getElementById('demo20').innerHTML = p;
+}
+// demo
+function fre(){
+	let x = "5" + 9 + 5;
+	document.getElementById('demo21').innerHTML = x;
+}
+// demo
+function fr(){
+	let x = 9 + 5 + "5";
+	document.getElementById('demo22').innerHTML = x;
+}
+// demo
+function f(){
+	let $$$ = 2;
+	let $myMoney = 5;
+document.getElementById("demo23").innerHTML = $$$ + $myMoney;
+}
+// demo
+function pubg(){
+	let $$$ = 2;
+	let $myMoney = 5;
+document.getElementById("demo24").innerHTML = $$$ + $myMoney;
+}
+// demo
+function pub(){
+	let x = 2;
+	let y = 5;
+	let z = x + y;
+document.getElementById("demo25").innerHTML = "The sum of x + y is: " + z;
+}
+// demo
+function pu(){
+	let a = 3;
+	let x = (100 + 50) * a;
+document.getElementById("demo26").innerHTML = x;
+}
+// demo
+function p(){
+	let x = 5;
+	let y = 3;
+	let z = x * y;
+document.getElementById("demo27").innerHTML = z;
+}
+// demo
+function freelancing(){
+	let x = 5;
+	let y = 3;
+	let z = x % y
+document.getElementById("demo28").innerHTML = z;
+}
+// demo
+function freelancin(){
+    try {
+    const PI = 3.141592653589793;
+    PI = 3.14;
+    }
+    catch (err) {
+    document.getElementById("demo29").innerHTML = err;
+    }
+}
+// demo
+function freelanci(){
+	let x = 6;
+	x++;
+	let z = x;
+document.getElementById("demo30").innerHTML = z;
+}
+// demo
+function freelanc(){
+	let x = 6;
+	x--;
+	let z = x;
+document.getElementById("demo31").innerHTML = z;
+}
+// demo
+function freelan(){
+	let x = 6;
+document.getElementById("demo32").innerHTML = x ** 2;
+}
+// demo
+function freela(){
+	let x = 6;
+document.getElementById("demo33").innerHTML = Math.pow(x,2);
 }
